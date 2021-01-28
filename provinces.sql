b12b3a8b6da7bdeae47140a154606062922b0857
Ivana <thianthiaan@gmail.com>
2021-01-28 13:07:28 +0700

    Update sql

--- provinces.sql
+++ provinces.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 28, 2021 at 03:21 AM
+-- Generation Time: Jan 28, 2021 at 07:04 AM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `provinces` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `recovered` int(11) NOT NULL,
   `death` int(11) NOT NULL,
@@ -41,10 +42,30 @@ CREATE TABLE `provinces` (
 -- Dumping data for table `provinces`
 --
 
-INSERT INTO `provinces` (`name`, `recovered`, `death`, `positive`, `url`, `isDeleted`) VALUES
-('DKI Jakarta', 83338, 2120, 98206, '/api/v1/provinces/1', 0),
-('Jawa Timur', 43671, 3606, 49801, '/api/v1/provinces/2', 0),
-('Jawa Barat', 21371, 616, 31907, '/api/v1/provinces/3', 0);
+INSERT INTO `provinces` (`id`, `name`, `recovered`, `death`, `positive`, `url`, `isDeleted`) VALUES
+(1, 'DKI Jakarta', 83338, 2120, 98206, '/api/v1/provinces/1', 0),
+(2, 'Jawa Timur', 43671, 3606, 49801, '/api/v1/provinces/2', 0),
+(3, 'Jawa Barat', 21371, 616, 31907, '/api/v1/provinces/3', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `provinces`
+--
+ALTER TABLE `provinces`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `provinces`
+--
+ALTER TABLE `provinces`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
